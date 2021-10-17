ee040812987461326cc8089c3432075d6c1a3171
mathyber <ilan23f@gmail.com>
2021-10-17 17:16:25 +0300

    оаипалатпотиолитпаолти

--- routes/imageGenerator.js
+++ routes/imageGenerator.js
@@ -11,12 +11,22 @@ router.get('/generator', async function (request, response) {
   response.end(image, 'binary');
 });
 
+// router.post('/generator', urlencodedParser, async function (request, response) {
+//   if(!request.body) return response.sendStatus(400);
+//   console.log(request.body);
+//   let image = await generator(request.body);
+//   response.writeHead(200, { 'Content-Type': 'image/png' });
+//   response.end(image, 'binary');
+// });
+
 router.post('/generator', urlencodedParser, async function (request, response) {
   if(!request.body) return response.sendStatus(400);
   console.log(request.body);
   let image = await generator(request.body);
-  response.writeHead(200, { 'Content-Type': 'image/png' });
-  response.end(image, 'binary');
+  //response.writeHead(200, { 'Content-Type': 'image/png' });
+  let data = "data:" + 'image/png' + ";base64," + Buffer.from(image).toString('base64');
+  console.log(data)
+  response.json({data: data});
 });
 
 module.exports = router;
